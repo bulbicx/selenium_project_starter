c620b6e3d58f97a3a09528dd43a0b88990c475c3
Marco Castellana <arkanoidmark96@gmail.com>
2021-07-29 20:13:50 +0100

    MC: Added functionality for buttons redirect

--- webProject/js/products.js
+++ webProject/js/products.js
@@ -11,16 +11,31 @@
   const displayProductsList = (data) => {
     let list = "<div class=\"row\">";
     for (let i = 0; i < data.length; i++) {
-      list += `
-      <div class="card">
-        <h5 class="card-title">${data[i].name}</h5>
-        <img src=${data[i].image_url} class="card-img-top" alt=${data[i].description}>
-        <div class="card-body">
-          <p class="card-text">${data[i].tagline}</p>
+      //for the first 4 products add button link and redirect to product pages related
+      if (i < 4) { 
+        list += `
+          <div class="card">
+            <h5 class="card-title">${ data[i].name }</h5>
+            <img src=${ data[i].image_url } class="card-img-top" alt=${ data[i].description }>
+            <div class="card-body">
+              <p class="card-text">${ data[i].tagline }</p>
+            </div>
+            <a href="product${data[i].id}.html" class="btn btn-primary">Read more...</a>
           </div>
-          <a href="#" class="btn btn-primary">Read more...</a>
-        </div>
-      `
+        `;
+      } else {
+        list += `
+          <div class="card">
+            <h5 class="card-title">${ data[i].name }</h5>
+            <img src=${ data[i].image_url } class="card-img-top" alt=${ data[i].tagline }>
+            <div class="card-body">
+              <p class="card-text">${ data[i].tagline }</p>
+            </div>
+            <a href="#" class="btn btn-primary">Read more...</a>
+          </div>
+        `;
+      }
+
       
     }
     list += "</div>";
